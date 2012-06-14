34a3b298ecf3700482329416076bbefe4881625d
Antonio Antillon <antillas21@gmail.com>
2012-06-14 11:42:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geostore::Application.config.secret_token = 'e7e38a332c78193094346438a8f5d9a84c74a467c375b68b0e825603323c1659454dba1a486e1162e7f9d658cc2e89617d1f3868a8cf04304a6affbf7d79de93'
